88c90db0c7c2133e466951dc3b17e4952d30ca9a
dlins10 <diego_melo11@hotmail.com>
2023-01-17 01:58:27 -0300

    commit styled background

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import GlobalStyle from "./style/global"
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <>
+      <GlobalStyle/>
+      <App />
+    </>
   </React.StrictMode>
 );
 
---
+++ src/style/global.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+    body{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: #696969;
+    }
+`;
